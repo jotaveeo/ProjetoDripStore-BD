7f8b67c8b1f336eb42fcf328969741841e1042f8
jotavee-dev <joaovitorengcomp@edu.unifor.br>
2024-11-18 17:31:12 -0300

    Adiciona middleware CORS ao servidor Express

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import route from "./src/route/route.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use("/api", route);
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
